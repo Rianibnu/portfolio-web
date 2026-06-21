ccc2d136b6fdf018e8a513314a8e6b7c4a83dad7
Rian Ibnu Rizal <rianibnurizal1@gmail.com>
2026-06-21 22:54:53 +0700

    fix: use PrismAsyncLight to drastically reduce Next.js build time and memory usage

--- src/app/(public)/blog/[slug]/page.tsx
+++ src/app/(public)/blog/[slug]/page.tsx
@@ -9,7 +9,7 @@ import { prisma } from "@/lib/prisma";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeSlug from "rehype-slug";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { PrismAsyncLight as SyntaxHighlighter } from "react-syntax-highlighter";
 import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import ShareButtons from "@/components/ui/share-buttons";
 
